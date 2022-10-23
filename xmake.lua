8bd518775920a8ef89c2621a60500dc1fe6c4840
Wade <gao_ziyu@foxmail.com>
2022-10-23 15:18:45 +0800

    feat: 🎸 support linux compile options
    support linux compile options

--- xmake.lua
+++ xmake.lua
@@ -6,12 +6,20 @@ target("Kaleidoscope")
     set_targetdir("build")
     add_includedirs("include")
     add_files("src/**.cc")
-    add_cxxflags("-g -stdlib=libc++ -fuse-ld=lld -v")
+    add_cxxflags("-g -v")
     add_defines("USE_JIT")
 
     if is_plat("macosx") then
+        add_cxxflags("-stdlib=libc++ -fuse-ld=lld")
         add_includedirs("/opt/homebrew/opt/llvm/include")
         add_linkdirs("/opt/homebrew/opt/llvm/lib")
         add_links("LLVM")
         add_ldflags("--verbose")
+    elseif is_plat("linux") then
+        add_toolchains("gcc")
+        add_includedirs("/usr/include/llvm")
+        add_linkdirs("/usr/lib/llvm-15/lib")
+        add_links("LLVM")
+        add_ldflags("--verbose")
+    else
     end
